cd0eb47ec2e5932ba4688b23179c5a4ce2f3a513
Jultromix <julio.morenoshz@uanl.edu.mx>
2023-05-27 13:25:11 +0000

    ft.app turned back to original form

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import flet as ft
 
+
 def main(page):
 
-    title = ft.Text(value="Welcome to EcoSort!",
-                    font_family="Consolas",
-                    size=72,
-                    text_align= ft.TextAlign.CENTER)
-    
-    boton = ft.ElevatedButton("Hola")
+  title = ft.Text(value="Welcome to EcoSort!",
+                  font_family="Consolas",
+                  size=72,
+                  text_align=ft.TextAlign.CENTER)
 
-    def btn_click(e):
-        pass
+  boton = ft.ElevatedButton("Hola")
 
-    page.add(
-        title,boton,
-    )
+  def btn_click(e):
+    pass
 
-ft.app(target=main, view=ft.WEB_BROWSER)
+  page.add(
+    title,
+    boton,
+  )
+
+
+ft.app(target=main)
